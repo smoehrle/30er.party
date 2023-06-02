1b3459ce1a2c4bfbb13ecc7a000427342912d475
smoehrle <26185187+smoehrle@users.noreply.github.com>
2023-06-02 20:36:59 +0200

    Merge active games and new games

--- website/views.py
+++ website/views.py
@@ -74,6 +74,12 @@ class NewGame(CreateView):
     form_class = PlayGameForm
     success_url = "/game-results/{id}"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['game_instances'] = PlayGame.objects.filter(finished=False).order_by("-time_stamp").all()
+        return context
+
+
 
 class Scores(TemplateView):
     template_name = "scores.html"
@@ -102,15 +108,6 @@ class Scores(TemplateView):
 
 
         context["players"] = sorted(players.values(), key=lambda x: x[3], reverse=True)
-        return context
-
-
-class ActiveGames(TemplateView):
-    template_name = "active_games.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['game_instances'] = PlayGame.objects.filter(finished=False).all()
         return context
 
 
